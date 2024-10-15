250b40af7d3276c516bb1392e45e266f1a96f212
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-10-15 16:46:50 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP FIRST PROJECT</title>
+</head>
+<body>
+    <div class="container">
+        <h1>my first php project with mysql</h1>
+    </div>
+      
+    <?php
+      echo "hello dhiraj bhai";       
+    ?>
+</body>
+</html>
